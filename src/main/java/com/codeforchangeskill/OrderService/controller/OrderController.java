1e674ffc929ad2e4827e42cdf0fadaf22971c11f
Mohammad Hifzan <mhifzan0707@gmail.com>
2025-02-24 14:01:19 +0530

    RESTTemplate used to get orderdetails succesffully and tested on Postman

--- src/main/java/com/codeforchangeskill/OrderService/controller/OrderController.java
+++ src/main/java/com/codeforchangeskill/OrderService/controller/OrderController.java
@@ -2,16 +2,14 @@ package com.codeforchangeskill.OrderService.controller;
 
 import com.codeforchangeskill.OrderService.external.client.ProductService;
 import com.codeforchangeskill.OrderService.model.OrderRequest;
+import com.codeforchangeskill.OrderService.model.OrderResponse;
 import com.codeforchangeskill.OrderService.service.OrderService;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -28,8 +26,20 @@ public class OrderController {
         long orderId=orderService.placeOrder(orderRequest);
         log.info("OrderId :{}",orderId);
         return new ResponseEntity<>(orderId, HttpStatus.OK);
-
-
     }
+    // Here we will be using RESTTEMPLATE instead of FEIGNCLIENT to get order details
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId){
+
+        //Remember it shows function need to be static issue if in place of orderService its class OrderService is added;
+        OrderResponse orderResponse
+                =orderService.getOrderDetails(orderId);
+
+
+        return new ResponseEntity<>(orderResponse,
+                HttpStatus.OK);
+    }
+
 
 }
